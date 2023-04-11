0df7b7406eda0ac37c5f0384b01bdb321bb4c914
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-04-11 18:12:02 -0400

    added correct paths in navbar

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 })
+
+
+// prevent defaut behavior when button is submitted 
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault()
+       });
